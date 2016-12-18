539b1c6911234a1b8d445e1bfa206d65960ff01b
Dmitriy Bobir <astercassio@gmail.com>
2016-12-18 15:34:45 +0100

    Added population initialization

--- genetic_algorithms.py
+++ genetic_algorithms.py
@@ -3,26 +3,29 @@ import random
 
 class IndividualGA:
     """
-    The class represents an element of population in GA.
+    The class represents an individual of population in GA.
     """
     def __init__(self, individ, fitness_val):
         """
+        A constructor.
 
         Args:
-            individ (list): A chromosome represented a solution. This chromosome is binary encoded (True, False).
+            individ (list): A chromosome represented a solution. The solution is binary encoded in chromosome.
             fitness_val (int): Fitness value of the given chromosome.
         """
-        self.individ = individ
+        self.individ = list(individ)
         self.fitness_val = fitness_val
 
 
 class GeneticAlgorithms:
-    def __init__(self, data, selection="rank", mut_prob=0.5, mut_type=1, cross_prob=0.95, cross_type=1, elitism=False,
-                 tournament_size=None):
+    def __init__(self, data=None, fitness_func=None, selection="rank", mut_prob=0.5, mut_type=1,
+                 cross_prob=0.95, cross_type=1, elitism=False, tournament_size=None):
         """
         Args:
-            data (list): A list with elements of the original population. This list will be binary encoded
-                (with True, False) later in order to indicate currently evaluated combination of elements.
+            data (list): A list with elements whose combination will be binary encoded and
+                evaluated by a fitness function. Minimum amount of elements is 4.
+            fitness_func (function): This function must compute fitness value of an input combination of the given data.
+                Function parameters must be: list of used indices of the given data (from 0), list of data itself.
             selection (str): Parent selection type. May be "rank" (Rank Wheel Selection),
                 "roulette" (Roulette Wheel Selection) or "tournament".
             tournament_size (int): Defines the size of tournament in case of 'selection' == 'tournament'.
@@ -36,6 +39,7 @@ class GeneticAlgorithms:
             elitism (True, False): Elitism on/off.
         """
         self.data = data
+        self.fitness_func = fitness_func
         self.selection = selection
         self.tournament_size = tournament_size
         self.mutation_prob = mut_prob
@@ -48,7 +52,8 @@ class GeneticAlgorithms:
         self._check_parameters()
 
     def _check_parameters(self):
-        if len(self.data) < 2 or \
+        if self.data is None or len(self.data) < 4 or \
+                self.fitness_func is None or \
                 self.mutation_prob < 0 or self.mutation_prob > 100 or \
                 self.mut_type < 1 or self.mut_type > len(self.data) or \
                 self.crossover_prob < 0 or self.crossover_prob > 100 or \
@@ -248,6 +253,29 @@ class GeneticAlgorithms:
             print('Unknown selection type:', self.selection)
             raise ValueError
 
+    def _get_bit_positions(self, number):
+        """
+        This function gets a decimal integer number and returns positions of bit 1 in
+        its binary representation. However, these positions are transformed the following way: they
+        are mapped on the data list (self.data) "as is". It means that LSB (least significant bit) is
+        mapped on the last position of the data list (e.g. len(self.data) - 1), MSB is mapped on
+        the first position of the data list (e.g. 0) and so on.
+
+        Args:
+            number (int): This decimal number represents binary encoded combination of the input data (self.data).
+        Returns:
+             list of positions with bit 1 (these positions are mapped on the input data list "as is" and thus,
+             LSB is equal to index (len(self.data) - 1) of the input data list).
+        """
+        binary_list = []
+        number_of_bits = len(self.data)
+
+        for i in range(number_of_bits):
+            if number & (1 << i):
+                binary_list.append(number_of_bits - 1 - i)
+
+        return binary_list
+
     def init_population(self, new_population):
         """
         Initializes population with the value of 'new_population'.
@@ -255,26 +283,47 @@ class GeneticAlgorithms:
         Args:
             new_population (list): New initial population of IndividualGA objects.
         """
-        self.population = list(new_population)
+        if not new_population:
+            print('New population is empty')
+            raise ValueError
+
+        self.population = []
+        for individ in new_population:
+            fit_val = self.fitness_func(individ, self.data)
+            self.population.append(IndividualGA(individ, fit_val))
 
     def init_random_population(self, size=None):
         """
         Initializes a new random population of the given size.
 
         Args:
-            size (int): Size of a new random population. If None, the size is set to len(self.data).
+            size (int): Size of a new random population. If None, the size is set to (2**len(self.data)) / 10, because
+                len(self.data) is a number of bits. Thus, a new population of size 10% of all possible solutions
+                (or of size 4 in case of len(self.data) < 5) will be created.
         """
+        max_num = 2 ** len(self.data)
+
         if size is None:
-            size = len(self.data)
+            if max_num < 20:
+                size = 4
+            else:
+                # 10% of all possible solutions
+                size = max_num // 10
         elif 2 > size > len(self.data):
             print('Wrong size of population:', size)
             raise ValueError
 
         # generate population
-        max_num = 2**len(self.data) - 1
-        self.population = self._random_diff(max_num + 1, size, start=1)
+        number_list = self._random_diff(max_num, size, start=1)
 
-    def run(self, population=None):
+        self.population = []
+        for num in number_list:
+            individ = self._get_bit_positions(num)
+            fit_val = self.fitness_func(individ, self.data)
+
+            self.population.append(IndividualGA(individ, fit_val))
+
+    def run(self):
         # fitness_sum = sum(ind.fitness_val for ind in population)
         # TODO
         pass
